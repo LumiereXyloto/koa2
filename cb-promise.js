4828857fceead74b2e9156e1e99c9ad60e2cd544
Lumiere Xyloto <740207968@qq.com>
2018-12-18 22:54:06 +0800

    create koa2 repository

---
+++ cb-promise.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+
+//回调函数
+// fs.readFile('./package.json', (err, data) => {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   data = JSON.parse(data)
+//   console.log(data.name)
+// })
+
+//过渡时期
+// function readFileAsync (path) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(path, (err, data) => {
+//       if (err) {
+//         reject(err)
+//       } else {
+//         resolve(data)
+//       }
+//     })
+//   })
+// }
+
+// readFileAsync('./package.json')
+//   .then(data => {
+//     data = JSON.parse(data)
+//     console.log(data.name)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
+
+
+  const util = require('util')
+
+  util.promisify(fs.readFile)('./package.json')
+    .then(JSON.parse)
+    .then(data => {
+      console.log(data.name)
+    })
+    .catch(err => {
+      console.log(err)
+    })
